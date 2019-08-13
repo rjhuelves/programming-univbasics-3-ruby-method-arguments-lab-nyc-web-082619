7658ad7cb702bd13bb95735b9fd7f4f8c9d5110f
Richard Huelves <rhuelves@gmail.com>
2019-08-13 21:46:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Richard, Ruby")
+introduction_with_language("Richard", "Ruby")
 
 
 
